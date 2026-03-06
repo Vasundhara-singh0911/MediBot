82f0980d199eca18a9b3790df8ac139a0cd167ac
Vasundhara Singh <vasu9259@gmail.com>
2026-03-07 01:37:35 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Vasundhara Singh",
+    author_email="vasundhara.singh0911@gmail.com",
+    packages=find_packages(),
+    intall_requires=[]
+)
